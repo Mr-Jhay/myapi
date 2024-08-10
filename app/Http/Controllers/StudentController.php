a0e1020b1c57ca84def51f34b08c08b5f837d8d7
yannah2003 <julianamarie373@gmail.com>
2024-08-10 11:40:40 +0800

    ..

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule; 
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\UserController;
-
+use Illuminate\Support\Facades\Auth;
 class StudentController extends Controller
 {
     public function store2(Request $request)
